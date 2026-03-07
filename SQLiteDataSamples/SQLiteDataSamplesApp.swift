5ec8ba955244156eef7c06ef15c8e98a0d68e603
gukt <29283212@qq.com>
2026-03-07 11:21:43 +0800

    Initial Commit

---
+++ SQLiteDataSamples/SQLiteDataSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteDataSamplesApp.swift
+//  SQLiteDataSamples
+//
+//  Created by Gu Kaitong  on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteDataSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SQLiteDataSamplesTests/SQLiteDataSamplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteDataSamplesTests.swift
+//  SQLiteDataSamplesTests
+//
+//  Created by Gu Kaitong  on 2026/3/7.
+//
+
+import Testing
+@testable import SQLiteDataSamples
+
+struct SQLiteDataSamplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
